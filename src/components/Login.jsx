11aa50dfbbdfe4455c1940d676396704497d4eb7
GlendyT <glendytuyuc@gmail.com>
2024-03-17 21:01:38 -0600

    get top artist

---
+++ src/components/Login.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+
+const LastFmUserInfo = () => {
+  const [userInfo, setUserInfo] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [usernameInput, setUsernameInput] = useState("");
+  const [error, setError] = useState("");
+
+  const fetchUserInfo = async () => {
+    try {
+      const apiKey = `${process.env.REACT_APP_API_KEY}`;
+      //const url = `https://ws.audioscrobbler.com/2.0/?method=user.getinfo&user=${usernameInput}&api_key=${apiKey}&format=json`;
+      const url = `${process.env.REACT_APP_PUBLIC_URL}?method=user.gettopartists&user=${usernameInput}&api_key=${apiKey}&format=json`;
+
+      const response = await fetch(url);
+      const data = await response.json();
+
+      if (data.error) {
+        // Handle error if there is one
+        setError(data.message);
+      } else {
+        //setUserInfo(data.topartists); //.artist[0]
+        const topArtists = data.topartists.artist.slice(0, 8);
+        setUserInfo(topArtists);
+      }
+    } catch (error) {
+      console.error("Error fetching user info:", error);
+      setError("Error fetching user info. Please try again later.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setError("");
+
+    fetchUserInfo();
+  };
+
+  console.log(userInfo);
+
+  return (
+    <form onSubmit={handleSubmit} className="text-center">
+      <input
+        type="text"
+        value={usernameInput}
+        onChange={(e) => setUsernameInput(e.target.value)}
+        placeholder="Enter Last.fm username"
+        required
+      />
+      <button type="submit">Get User Info</button>
+
+      {loading && <p>Loading...</p>}
+
+      {error && <p>Error: {error}</p>}
+
+      {userInfo ? (
+        <>
+          <p>Most listened artist:</p>
+          <p>
+            {" "}
+            {userInfo.map((artist, index) => (
+              <li key={index}>
+                Artist Name: {artist.name} {""}
+                All time Scrobles: {artist.playcount}
+                
+              </li>
+              
+            ))}
+          </p>
+        </>
+      ) : (
+        <p>No user information found</p>
+      )}
+    </form>
+  );
+};
+
+export default LastFmUserInfo;
--- src/hooks/useLastfm.jsx
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import LasfmContext from "../context/LasfmProvider";
-
-const useLastfmd =  () => {
-    return useContext(LasfmContext)
-}
-
-export default useLastfmd;
